3cfbb2c5a3cfc6849b06371fdceede1d3c0c373d
moursbourn1 <moursbourn@gmail.com>
2023-12-21 16:44:06 -0800

    Update with Hostname and Timecode

--- stats_small.py
+++ stats_small.py
@@ -6,43 +6,74 @@ import time
 import board
 import busio
 import digitalio
-import socket
+import time
 
 from PIL import Image, ImageDraw, ImageFont
 import adafruit_ssd1306
 
 import subprocess
+import time
 
 from datetime import datetime, timedelta
-import pytz
 import re
 
 def get_timecode_milliseconds():
-    # Get the current time in UTC
-    utc_now = datetime.now(pytz.utc)
+  # current time in seconds
+  sec = datetime.now().timestamp()
+  #print('Time in seconds:', sec)
 
-    # Convert to Pacific Standard Time (PST)
-    pst_timezone = pytz.timezone('America/Los_Angeles')
-    pst_now = utc_now.astimezone(pst_timezone)
+  # convert to Days, hh:mm:ss
+  td = str(timedelta(seconds=sec))
+  #print(td)
+  td = re.search(r",(.*)", td) 
+  return str(td.group(1))
 
-    # Extract the hour, minute, second, and millisecond components from the PST time
-    hour = pst_now.hour
-    minute = pst_now.minute
-    second = pst_now.second
-    millisecond = pst_now.microsecond // 1000
 
-    # Format the components as HH:MIN:SEC:MS
-    time_str = f'{hour:02d}:{minute:02d}:{second:02d}:{millisecond:03d}'
+def x_get_timecode_milliseconds():
+  """Gets the timecode in milliseconds in Linux using Python.
 
-    return time_str
-hostname = socket.gethostname()
+  Returns:
+    A string representing the timecode in milliseconds in the format HH:MM:SS.XXX.
+  """
+
+  # Get the current time in seconds since the epoch.
+  seconds_since_epoch = time.time()
+
+  # Convert the seconds since the epoch to milliseconds.
+  milliseconds_since_epoch = seconds_since_epoch * 1000
+
+  # Get the timecode in HH:MM:SS format.
+  timecode_hhmmss = time.strftime("%H:%M:%S", time.gmtime(seconds_since_epoch))
+
+  # Split the timecode into hours, minutes, and seconds.
+  hours, minutes, seconds = timecode_hhmmss.split(":")
+
+  # Convert the seconds to milliseconds.
+  milliseconds = int(seconds) * 1000
+
+  # Add the milliseconds to the timecode in HH:MM:SS format.
+  timecode_milliseconds = "%s.%03d" % (timecode_hhmmss, milliseconds)
+
+  return str(timecode_milliseconds)
+
+# Get the timecode in milliseconds.
+timecode_milliseconds = get_timecode_milliseconds()
+
+# Print the timecode in milliseconds.
+print(timecode_milliseconds)
+
+def old_get_timecode_milliseconds():
+  """Get the current time in milliseconds."""
+  return str(int(time.perf_counter_ns() / 1000000))
+
+print(get_timecode_milliseconds())
 
 # Define the Reset Pin
 oled_reset = digitalio.DigitalInOut(board.D4)
 
 # Display Parameters
 WIDTH = 128
-HEIGHT = 64
+HEIGHT = 32
 BORDER = 5
 
 # Display Refresh
@@ -80,27 +111,20 @@ while True:
     cmd = "top -bn1 | grep load | awk '{printf \"CPU: %.2f\", $(NF-2)}'"
     CPU = subprocess.check_output(cmd, shell = True )
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    # MemUsage = subprocess.check_output(cmd, shell = True )
-    # cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-
-    Hostname = socket.gethostname()
-
+    MemUsage = subprocess.check_output(cmd, shell = True )
+    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
     Disk = subprocess.check_output(cmd, shell = True )
     cmd = "vcgencmd measure_temp |cut -f 2 -d '='"
     Temp = subprocess.check_output(cmd, shell = True) 
     timecode = get_timecode_milliseconds()
     
     # Pi Stats Display
-    draw.text((0, 0), "IP: " + str(IP,'utf-8'), font=font, fill=255)
-    draw.text((0, 16), (f'Name: {Hostname}'), font=font, fill=255)
-
-    draw.text((0, 32), str(CPU,'utf-8') + "LA", font=font, fill=255)
-    draw.text((80, 32), str(Temp,'utf-8') , font=font, fill=255)
-
-    # draw.text((0, 32), str(MemUsage,'utf-8'), font=font, fill=255)
+    #draw.text((0, 0), "IP: " + str(IP,'utf-8'), font=font, fill=255)
+    #draw.text((0, 16), str(CPU,'utf-8') + "LA", font=font, fill=255)
+    #draw.text((80, 16), str(Temp,'utf-8') , font=font, fill=255)
+    #draw.text((0, 32), str(MemUsage,'utf-8'), font=font, fill=255)
     #draw.text((0, 48), str(Disk,'utf-8'), font=font, fill=255)
-
-    draw.text((0, 48),f'TC: {timecode}', font=font, fill=255)
+    draw.text((0, 0),timecode, font=font, fill=255)
         
     # Display image
     oled.image(image)
